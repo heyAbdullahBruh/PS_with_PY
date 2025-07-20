f9791988f6257b26cf426a371c0469c6ff987da9
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-07-20 20:27:24 +0600

    Problem-16-- find secondlarge number in list

---
+++ 16secoundLargNum.py
@@ -0,0 +1,30 @@
+#Promlem-16---> Find second largest numb from list ---> 
+
+numList =[3,4,68,5,65,32,88]
+
+# way ---> 1 
+# def check2ndLargNum(listN) :
+#     secLargNum=0
+#     largeNum=0
+#     for i in range(len(listN)):
+#         if listN[i] > largeNum:
+#             largeNum=listN[i]
+#     for j in range(len(listN)):
+#         if listN[j] > secLargNum and largeNum !=listN[j] :
+#             secLargNum=listN[j]
+#     return {"largeNum":largeNum,"secLargNum":secLargNum,}
+
+# way ---> 2 
+def check2ndLargNum(listN) :
+    secLargNum=0
+    largeNum=0
+    for i in listN:
+        if i > largeNum:
+            largeNum=i
+    for j in listN:
+        if j > secLargNum and largeNum !=j :
+            secLargNum=j
+    return {"largeNum":largeNum,"secLargNum":secLargNum,}
+
+print(check2ndLargNum(numList))
+
