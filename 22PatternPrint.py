642f00dc8b33846ef1e09eaf58f5951cba90a2e3
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-07-24 15:31:44 +0600

    Problem-22-- Pattern priniting - Diamond'

--- 22PatternPrint.py
+++ 22PatternPrint.py
@@ -1,6 +1,14 @@
 # Problem-22 ---> Pattern Printing ( tringle and dimond) -->
-# Diamond
+# Tringle
 def tringlePattern(t):
     for r in range(1,t+1):
         print((2*r-1)*'*')
 tringlePattern(10) 
+
+# diamond
+def diamondPattern(t):
+    for r in range(1,t+1):
+        print((2*r-1)*'*')
+    for r in reversed(range(1,t+1)):
+        print((2*r-1)*'* ')
+diamondPattern(10) 
