6e2aa3d88369cd507cd76dadac3c20dc6de2e265
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-07-24 17:00:56 +0600

    Problem-26--  Search for a Word in a File and Print Line Numbers

---
+++ 26PrintWordLineNumbInFile/read.py
@@ -0,0 +1,17 @@
+#Problem-25 ---> Search for a Word in a File and Print Line Numbers
+
+
+
+def findWordLineNumformFile(filename,word):
+    fileTxt =open(filename, "r")
+    fileData = fileTxt.read()
+    txtToList = fileData.split('\n')
+    lineNumb=0
+    for l in txtToList:
+        if word in l :
+              lineNumb=txtToList.index(l)+1
+    return lineNumb
+   
+print(findWordLineNumformFile('file.txt',"blanditiis")) # 6
+
+
