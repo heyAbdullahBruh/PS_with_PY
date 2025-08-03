7edb69f3e53304429985fe7a96a1990adff5aaad
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-08-03 21:25:42 +0600

    #Problem35 make a area calculator with python class

--- 35AreaCalWithClasses.py
+++ 35AreaCalWithClasses.py
@@ -30,11 +30,28 @@ class Tringle (Item):
     def __init__(self,a,b):
         super().__init__(a,b)
     def calc(self):
+    # a= base , b= height
         return f"This Tringle Area is = {self.a*self.b*.5}"
-    def circumference(self):
-        return f"This Tringle circumference is = {2*self.a+self.b}"
-
+    def perimeter (self):
+    # a= base , b=  sides
+        return f"This Tringle perimeter  is = {2*self.a+self.b}"
 tringle1= Tringle(3.1618,5)
 print(tringle1.calc()) #This Tringle Area is = 7.9045
-print(tringle1.circumference())#This Tringle circumference is = 11.323599999999999
+print(tringle1.perimeter ())#This Tringle perimeter  is = 11.323599999999999
+
+
+class Traphiziam(Item):
+     def __init__(self,a,b,c,d,h):
+        super().__init__(a,b)
+        self.c=c
+        self.d=d
+        self.h=h
+     def calc(self):
+         return f"This Traphiziam Area is = {.5*(self.a+self.b)*self.h}"
+     def perimeter (self):
+        return f"This Tringle perimeter  is = {self.a+self.b+self.c+self.d}"
+
+trapezium1= Tringle(3,7,5,8)
+print(trapezium1.calc()) #This Tringle Area is = 7.9045
+print(trapezium1.perimeter ())#This Tringle perimeter  is = 11.323599999999999
 
