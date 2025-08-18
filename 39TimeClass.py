a24cbe5a081d10ae7ef469e6c72d8b5f8081f935
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-08-18 19:42:42 +0600

    #Problem-39 make time  class

---
+++ 39TimeClass.py
@@ -0,0 +1,28 @@
+# Problem -39 --- Time Class --> 
+
+from math import floor
+
+class Time :
+    def __init__(self,hours, minutes):
+          self.hours=hours
+          self.minutes=minutes
+    def add_time(self, hours, minutes):
+        minutesSum = self.minutes + minutes
+        hoursSum = self.hours + hours
+
+        if minutesSum >= 60 :
+            self.hours = hoursSum + floor(minutesSum/60)
+            self.minutes = floor(minutesSum%60)
+            return
+        self.hours = hoursSum 
+        self.minutes = minutesSum
+        
+    def display(self) :
+        return f"Running time is {self.hours} : {self.minutes:02d} "
+
+time1= Time(4,30)    
+print(time1.display())
+
+time1.add_time(4,31)
+print(time1.display())
+
